4545c0cba7a5141b15bf6e061ac54194455c273c
cwegescheide <98625175+cwegescheide@users.noreply.github.com>
2024-08-19 15:25:04 -0400

    Update streamlit_app.py
    Trying to correct python package import issues..

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 # Import python packages
-import streamlit as st
-import Pandas as pd
+import streamlit as st from snowflake.snowpark.functions import col
+import Pandas as pd from snowflake.snowpark.functions import col
 #from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
+
 import requests
 
 

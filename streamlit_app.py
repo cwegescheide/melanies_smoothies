3e34840a18cded83fdea51eac4b6c8b14feff7d5
cwegescheide <98625175+cwegescheide@users.noreply.github.com>
2024-08-19 12:00:15 -0400

    Update streamlit_app.py
    Edited the session to use cnx.session()

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-cnx = st.connection("snowflake")
-session = cnx.session()
+#session = cnx.session()
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -16,7 +15,9 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:') 
 st.write('The name on your Smoothie will be:', name_on_order)
 
-session = get_active_session()
+#session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 ingredients_string = ''
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)

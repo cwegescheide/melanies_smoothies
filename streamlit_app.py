0cd52e14f2559a9b61b5752f370559f379d65c81
cwegescheide <98625175+cwegescheide@users.noreply.github.com>
2024-08-19 15:38:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
-import streamlit as st from snowflake.snowpark.functions import col
-import Pandas as pd from snowflake.snowpark.functions import col
+import streamlit as st 
+import Pandas as pd 
+from snowflake.snowpark.functions import col
 #from snowflake.snowpark.context import get_active_session
 
 import requests

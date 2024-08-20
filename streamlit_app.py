7fe1780fcb851678c92495679ac6fe7f0cdc6994
cwegescheide <98625175+cwegescheide@users.noreply.github.com>
2024-08-19 20:20:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,9 @@
 # Import python packages
-import streamlit as st 
-import Pandas as pd 
+import streamlit as st
 from snowflake.snowpark.functions import col
+import Pandas as pd 
 #from snowflake.snowpark.context import get_active_session
-
 import requests
-
-
 
 #session = cnx.session()
 

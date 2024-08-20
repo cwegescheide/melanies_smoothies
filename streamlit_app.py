85d0a2a6d125f057550282e2bc4aff7c4707b33e
cwegescheide <98625175+cwegescheide@users.noreply.github.com>
2024-08-19 21:02:31 -0400

    Update streamlit_app.py
    Fixed indentation issue on "search_on" line

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
          
           st.subheader(fruit_chosen + ' Nutrition Information')
           #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-           fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+          fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
           #st.text(fruityvice_response.json())
           fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
